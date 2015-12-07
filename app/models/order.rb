c6baa43dbf1d8a9859f83a92d5563b4ade73b629
Nguyễn Nhật Hoàng <hoangnn93@gmail.com>
2015-12-07 21:21:47 +0700

    Update validate order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ActiveRecord::Base
   belongs_to :dish
 
+  validates :name, :presence => true
+  validates :address, :presence => true
+  validates :phone, :presence => true
+
   attr_accessor :coupon_code
 end
